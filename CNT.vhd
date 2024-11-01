1a14e55a8c402fb77249b5a9b29e9eead0bb79fe
Piotr Rosiński <piotr.rosinski18@gmail.com>
2024-11-01 15:58:42 +0100

    formatted the code

--- CNT.vhd
+++ CNT.vhd
@@ -1,25 +1,32 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
 entity counter is
-port (RESET : in std_logic;
-CLK : in std_logic;
-Y : out std_logic_vector (7 downto 0));
-end counter;
+	port (
+	RESET : in std_logic;
+	CLK : in std_logic;
+	Y : out std_logic_vector (7 downto 0));
+	end counter;
+
 architecture behavioral of counter is
 signal CNT: unsigned(7 downto 0);
 begin
 
 process (RESET, CLK)
 begin
+
 if RESET = '1' then
 CNT <= (others => '0');
+
 elsif rising_edge(CLK) then
--- Na zboczu narastającym sygnału zegarowego
--- sygnał CNT reprezentujący stan licznika
--- jest inkrementowany
+
 CNT <= CNT + 1;
+
 end if;
+
 end process;
+
 Y <= std_logic_vector(CNT);
+
 end behavioral;
--- testCNT.vhd
+++ testCNT.vhd
@@ -1,19 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 entity testCNT is
 end testCNT;
+
 architecture behavior of testCNT is
 component counter
 port(
-RESET : in std_logic;
-CLK : in std_logic;
-Y : out std_logic_vector(7 downto 0)
+	RESET : in std_logic;
+	CLK : in std_logic;
+	Y : out std_logic_vector(7 downto 0)
 );
 end component;
+
 signal RESET : std_logic := '0';
 signal CLK : std_logic := '0';
 signal Y : std_logic_vector(7 downto 0);
--- Stała określająca okres zegara
+
 constant CLK_period : time := 10 ns;
 begin
 uut: counter port map (
@@ -21,7 +24,7 @@ RESET => RESET,
 CLK => CLK,
 Y => Y
 );
--- Proces generujący sygnał zegarowy
+
 CLK_process :process
 begin
 CLK <= '0';
@@ -29,15 +32,12 @@ wait for CLK_period/2;
 CLK <= '1';
 wait for CLK_period/2;
 end process;
--- Proces generujący sygnały sterujące
--- w tym przypadku tylko sygnał resetu
+
 stim_proc: process
 begin
 RESET <= '1';
 wait for 20 ns;
 RESET <= '0';
--- W przypadku bardziej złożonego sterowania należy tutaj,
--- po kolejnych instrukcjach wait for …, zadawać wektory testowe
 wait;
 end process;
 end;
